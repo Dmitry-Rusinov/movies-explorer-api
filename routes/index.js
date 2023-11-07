f9eceed2ac1433ecdfe062c74a6c0811216ff027
Dmitry Rusinov <kachek96@yandex.ru>
2023-11-07 20:54:18 +0300

    защитил роут signout авторизацией

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import movieRoutes from './movies.js';
 import adminsRouter from './admins.js';
 import auth from '../middlewares/auth.js';
 import NotFound from '../errors/NotFound.js';
+import { signout } from '../controllers/user.js';
 
 const router = Router();
 
@@ -13,6 +14,7 @@ router.use(auth);
 
 router.use('/', userRoutes);
 router.use('/', movieRoutes);
+router.get('/signout', signout);
 
 router.use('*', (req, res, next) => {
   next(new NotFound('Страница не найдена'));
